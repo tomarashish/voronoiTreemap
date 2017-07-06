e61b36b06df359be8719975d24ac05b17891b9ab
tomarashish <aashish.dyp@gmail.com>
2017-07-06 15:33:23 +0200

    add changes

---
+++ data/test2.js
@@ -0,0 +1,867 @@
+flare_json = {
+
+	"name": "flare",
+    children: [
+      {
+        name: "Cluster 1",
+        size: 1,
+        id: "0"
+      },
+      {
+        name: "Cluster 2",
+        size: 1,
+        children: [
+          {
+            name: "Cluster 2.1",
+            size: 1,
+            id: "2"
+          },
+          {
+            name: "Cluster 2.2",
+            size: 1,
+            children: [
+              {
+                name: "Cluster 2.2.1",
+                size: 1,
+                id: "4"
+              },
+              {
+                name: "Cluster 2.2.2",
+                size: 1,
+                id: "5"
+              }
+            ],
+            id: "3"
+          },
+          {
+            name: "Cluster 2.3",
+            size: 1,
+            id: "6"
+          },
+          {
+            name: "Cluster 2.4",
+            size: 1,
+            id: "7"
+          },
+          {
+            name: "Cluster 2.5",
+            size: 1,
+            children: [
+              {
+                name: "Cluster 2.5.1",
+                size: 1,
+                id: "9"
+              },
+              {
+                name: "Cluster 2.5.2",
+                size: 1,
+                id: "10"
+              },
+              {
+                name: "Cluster 2.5.3",
+                size: 1,
+                id: "11"
+              },
+              {
+                name: "Cluster 2.5.4",
+                size: 1,
+                id: "12"
+              }
+            ],
+            id: "8"
+          },
+          {
+            name: "Cluster 2.6",
+            size: 1,
+            id: "13"
+          }
+        ],
+        id: "1"
+      },
+      {
+        name: "Cluster 3",
+        size: 1,
+        children: [
+          {
+            name: "Cluster 3.1",
+            size: 1,
+            id: "15"
+          },
+          {
+            name: "Cluster 3.2",
+            size: 1,
+            children: [
+              {
+                name: "Cluster 3.2.1",
+                size: 1,
+                id: "17"
+              },
+              {
+                name: "Cluster 3.2.2",
+                size: 1,
+                id: "18"
+              },
+              {
+                name: "Cluster 3.2.3",
+                size: 1,
+                id: "19"
+              }
+            ],
+            id: "16"
+          },
+          {
+            name: "Cluster 3.3",
+            size: 1,
+            id: "20"
+          },
+          {
+            name: "Cluster 3.4",
+            size: 1,
+            children: [
+              {
+                name: "Cluster 3.4.1",
+                size: 1,
+                id: "22"
+              },
+              {
+                name: "Cluster 3.4.2",
+                size: 1,
+                id: "23"
+              },
+              {
+                name: "Cluster 3.4.3",
+                size: 1,
+                id: "24"
+              },
+              {
+                name: "Cluster 3.4.4",
+                size: 1,
+                id: "25"
+              },
+              {
+                name: "Cluster 3.4.5",
+                size: 1,
+                id: "26"
+              }
+            ],
+            id: "21"
+          },
+          {
+            name: "Cluster 3.5",
+            size: 1,
+            children: [
+              {
+                name: "Cluster 3.5.1",
+                size: 1,
+                id: "28"
+              },
+              {
+                name: "Cluster 3.5.2",
+                size: 1,
+                id: "29"
+              },
+              {
+                name: "Cluster 3.5.3",
+                size: 1,
+                id: "30"
+              },
+              {
+                name: "Cluster 3.5.4",
+                size: 1,
+                id: "31"
+              },
+              {
+                name: "Cluster 3.5.5",
+                size: 1,
+                id: "32"
+              }
+            ],
+            id: "27"
+          },
+          {
+            name: "Cluster 3.6",
+            size: 1,
+            id: "33"
+          }
+        ],
+        id: "14"
+      },
+      {
+        name: "Cluster 4",
+        size: 1,
+        children: [
+          {
+            name: "Cluster 4.1",
+            size: 1,
+            children: [
+              {
+                name: "Cluster 4.1.1",
+                size: 1,
+                id: "36"
+              },
+              {
+                name: "Cluster 4.1.2",
+                size: 1,
+                id: "37"
+              },
+              {
+                name: "Cluster 4.1.3",
+                size: 1,
+                id: "38"
+              },
+              {
+                name: "Cluster 4.1.4",
+                size: 1,
+                id: "39"
+              }
+            ],
+            id: "35"
+          },
+          {
+            name: "Cluster 4.2",
+            size: 1,
+            id: "40"
+          },
+          {
+            name: "Cluster 4.3",
+            size: 1,
+            id: "41"
+          },
+          {
+            name: "Cluster 4.4",
+            size: 1,
+            children: [
+              {
+                name: "Cluster 4.4.1",
+                size: 1,
+                id: "43"
+              },
+              {
+                name: "Cluster 4.4.2",
+                size: 1,
+                id: "44"
+              },
+              {
+                name: "Cluster 4.4.3",
+                size: 1,
+                id: "45"
+              }
+            ],
+            id: "42"
+          },
+          {
+            name: "Cluster 4.5",
+            size: 1,
+            children: [
+              {
+                name: "Cluster 4.5.1",
+                size: 1,
+                id: "47"
+              },
+              {
+                name: "Cluster 4.5.2",
+                size: 1,
+                id: "48"
+              },
+              {
+                name: "Cluster 4.5.3",
+                size: 1,
+                id: "49"
+              },
+              {
+                name: "Cluster 4.5.4",
+                size: 1,
+                id: "50"
+              }
+            ],
+            id: "46"
+          },
+          {
+            name: "Cluster 4.6",
+            size: 1,
+            children: [
+              {
+                name: "Cluster 4.6.1",
+                size: 1,
+                id: "52"
+              },
+              {
+                name: "Cluster 4.6.2",
+                size: 1,
+                id: "53"
+              },
+              {
+                name: "Cluster 4.6.3",
+                size: 1,
+                id: "54"
+              },
+              {
+                name: "Cluster 4.6.4",
+                size: 1,
+                id: "55"
+              },
+              {
+                name: "Cluster 4.6.5",
+                size: 1,
+                id: "56"
+              }
+            ],
+            id: "51"
+          },
+          {
+            name: "Cluster 4.7",
+            size: 1,
+            id: "57"
+          }
+        ],
+        id: "34"
+      },
+      {
+        name: "Cluster 5",
+        size: 1,
+        id: "58"
+      },
+      {
+        name: "Cluster 6",
+        size: 1,
+        children: [
+          {
+            name: "Cluster 6.1",
+            size: 1,
+            children: [
+              {
+                name: "Cluster 6.1.1",
+                size: 1,
+                id: "61"
+              },
+              {
+                name: "Cluster 6.1.2",
+                size: 1,
+                id: "62"
+              },
+              {
+                name: "Cluster 6.1.3",
+                size: 1,
+                id: "63"
+              }
+            ],
+            id: "60"
+          },
+          {
+            name: "Cluster 6.2",
+            size: 1,
+            id: "64"
+          },
+          {
+            name: "Cluster 6.3",
+            size: 1,
+            id: "65"
+          },
+          {
+            name: "Cluster 6.4",
+            size: 1,
+            children: [
+              {
+                name: "Cluster 6.4.1",
+                size: 1,
+                id: "67"
+              },
+              {
+                name: "Cluster 6.4.2",
+                size: 1,
+                id: "68"
+              },
+              {
+                name: "Cluster 6.4.3",
+                size: 1,
+                id: "69"
+              },
+              {
+                name: "Cluster 6.4.4",
+                size: 1,
+                id: "70"
+              },
+              {
+                name: "Cluster 6.4.5",
+                size: 1,
+                id: "71"
+              }
+            ],
+            id: "66"
+          }
+        ],
+        id: "59"
+      },
+      {
+        name: "Cluster 7",
+        size: 1,
+        children: [
+          {
+            name: "Cluster 7.1",
+            size: 1,
+            children: [
+              {
+                name: "Cluster 7.1.1",
+                size: 1,
+                id: "74"
+              },
+              {
+                name: "Cluster 7.1.2",
+                size: 1,
+                id: "75"
+              },
+              {
+                name: "Cluster 7.1.3",
+                size: 1,
+                id: "76"
+              }
+            ],
+            id: "73"
+          },
+          {
+            name: "Cluster 7.2",
+            size: 1,
+            children: [
+              {
+                name: "Cluster 7.2.1",
+                size: 1,
+                id: "78"
+              },
+              {
+                name: "Cluster 7.2.2",
+                size: 1,
+                id: "79"
+              },
+              {
+                name: "Cluster 7.2.3",
+                size: 1,
+                id: "80"
+              },
+              {
+                name: "Cluster 7.2.4",
+                size: 1,
+                id: "81"
+              }
+            ],
+            id: "77"
+          },
+          {
+            name: "Cluster 7.3",
+            size: 1,
+            id: "82"
+          },
+          {
+            name: "Cluster 7.4",
+            size: 1,
+            id: "83"
+          }
+        ],
+        id: "72"
+      },
+      {
+        name: "Cluster 8",
+        size: 1,
+        id: "84"
+      },
+      {
+        name: "Cluster 9",
+        size: 1,
+        id: "85"
+      },
+      {
+        name: "Cluster 10",
+        size: 1,
+        children: [
+          {
+            name: "Cluster 10.1",
+            size: 1,
+            id: "87"
+          },
+          {
+            name: "Cluster 10.2",
+            size: 1,
+            id: "88"
+          },
+          {
+            name: "Cluster 10.3",
+            size: 1,
+            id: "89"
+          },
+          {
+            name: "Cluster 10.4",
+            size: 1,
+            id: "90"
+          },
+          {
+            name: "Cluster 10.5",
+            size: 1,
+            children: [
+              {
+                name: "Cluster 10.5.1",
+                size: 1,
+                id: "92"
+              },
+              {
+                name: "Cluster 10.5.2",
+                size: 1,
+                id: "93"
+              },
+              {
+                name: "Cluster 10.5.3",
+                size: 1,
+                id: "94"
+              }
+            ],
+            id: "91"
+          }
+        ],
+        id: "86"
+      },
+      {
+        name: "Cluster 11",
+        size: 1,
+        id: "95"
+      },
+      {
+        name: "Cluster 12",
+        size: 1,
+        children: [
+          {
+            name: "Cluster 12.1",
+            size: 1,
+            id: "97"
+          },
+          {
+            name: "Cluster 12.2",
+            size: 1,
+            id: "98"
+          },
+          {
+            name: "Cluster 12.3",
+            size: 1,
+            children: [
+              {
+                name: "Cluster 12.3.1",
+                size: 1,
+                id: "100"
+              },
+              {
+                name: "Cluster 12.3.2",
+                size: 1,
+                id: "101"
+              },
+              {
+                name: "Cluster 12.3.3",
+                size: 1,
+                id: "102"
+              },
+              {
+                name: "Cluster 12.3.4",
+                size: 1,
+                id: "103"
+              }
+            ],
+            id: "99"
+          },
+          {
+            name: "Cluster 12.4",
+            size: 1,
+            children: [
+              {
+                name: "Cluster 12.4.1",
+                size: 1,
+                id: "105"
+              },
+              {
+                name: "Cluster 12.4.2",
+                size: 1,
+                id: "106"
+              },
+              {
+                name: "Cluster 12.4.3",
+                size: 1,
+                id: "107"
+              }
+            ],
+            id: "104"
+          },
+          {
+            name: "Cluster 12.5",
+            size: 1,
+            children: [
+              {
+                name: "Cluster 12.5.1",
+                size: 1,
+                id: "109"
+              },
+              {
+                name: "Cluster 12.5.2",
+                size: 1,
+                id: "110"
+              },
+              {
+                name: "Cluster 12.5.3",
+                size: 1,
+                id: "111"
+              },
+              {
+                name: "Cluster 12.5.4",
+                size: 1,
+                id: "112"
+              }
+            ],
+            id: "108"
+          },
+          {
+            name: "Cluster 12.6",
+            size: 1,
+            children: [
+              {
+                name: "Cluster 12.6.1",
+                size: 1,
+                id: "114"
+              },
+              {
+                name: "Cluster 12.6.2",
+                size: 1,
+                id: "115"
+              }
+            ],
+            id: "113"
+          },
+          {
+            name: "Cluster 12.7",
+            size: 1,
+            children: [
+              {
+                name: "Cluster 12.7.1",
+                size: 1,
+                id: "117"
+              },
+              {
+                name: "Cluster 12.7.2",
+                size: 1,
+                id: "118"
+              },
+              {
+                name: "Cluster 12.7.3",
+                size: 1,
+                id: "119"
+              },
+              {
+                name: "Cluster 12.7.4",
+                size: 1,
+                id: "120"
+              },
+              {
+                name: "Cluster 12.7.5",
+                size: 1,
+                id: "121"
+              }
+            ],
+            id: "116"
+          }
+        ],
+        id: "96"
+      },
+      {
+        name: "Cluster 13",
+        size: 1,
+        children: [
+          {
+            name: "Cluster 13.1",
+            size: 1,
+            id: "123"
+          },
+          {
+            name: "Cluster 13.2",
+            size: 1,
+            children: [
+              {
+                name: "Cluster 13.2.1",
+                size: 1,
+                id: "125"
+              },
+              {
+                name: "Cluster 13.2.2",
+                size: 1,
+                id: "126"
+              }
+            ],
+            id: "124"
+          },
+          {
+            name: "Cluster 13.3",
+            size: 1,
+            id: "127"
+          },
+          {
+            name: "Cluster 13.4",
+            size: 1,
+            children: [
+              {
+                name: "Cluster 13.4.1",
+                size: 1,
+                id: "129"
+              },
+              {
+                name: "Cluster 13.4.2",
+                size: 1,
+                id: "130"
+              },
+              {
+                name: "Cluster 13.4.3",
+                size: 1,
+                id: "131"
+              },
+              {
+                name: "Cluster 13.4.4",
+                size: 1,
+                id: "132"
+              },
+              {
+                name: "Cluster 13.4.5",
+                size: 1,
+                id: "133"
+              },
+              {
+                name: "Cluster 13.4.6",
+                size: 1,
+                id: "134"
+              }
+            ],
+            id: "128"
+          },
+          {
+            name: "Cluster 13.5",
+            size: 1,
+            id: "135"
+          },
+          {
+            name: "Cluster 13.6",
+            size: 1,
+            id: "136"
+          },
+          {
+            name: "Cluster 13.7",
+            size: 1,
+            children: [
+              {
+                name: "Cluster 13.7.1",
+                size: 1,
+                id: "138"
+              },
+              {
+                name: "Cluster 13.7.2",
+                size: 1,
+                id: "139"
+              },
+              {
+                name: "Cluster 13.7.3",
+                size: 1,
+                id: "140"
+              },
+              {
+                name: "Cluster 13.7.4",
+                size: 1,
+                id: "141"
+              }
+            ],
+            id: "137"
+          }
+        ],
+        id: "122"
+      },
+      {
+        name: "Cluster 14",
+        size: 1,
+        id: "142"
+      },
+      {
+        name: "Cluster 15",
+        size: 1,
+        children: [
+          {
+            name: "Cluster 15.1",
+            size: 1,
+            children: [
+              {
+                name: "Cluster 15.1.1",
+                size: 1,
+                id: "145"
+              },
+              {
+                name: "Cluster 15.1.2",
+                size: 1,
+                id: "146"
+              },
+              {
+                name: "Cluster 15.1.3",
+                size: 1,
+                id: "147"
+              },
+              {
+                name: "Cluster 15.1.4",
+                size: 1,
+                id: "148"
+              },
+              {
+                name: "Cluster 15.1.5",
+                size: 1,
+                id: "149"
+              }
+            ],
+            id: "144"
+          },
+          {
+            name: "Cluster 15.2",
+            size: 1,
+            id: "150"
+          },
+          {
+            name: "Cluster 15.3",
+            size: 1,
+            id: "151"
+          },
+          {
+            name: "Cluster 15.4",
+            size: 1,
+            children: [
+              {
+                name: "Cluster 15.4.1",
+                size: 1,
+                id: "153"
+              },
+              {
+                name: "Cluster 15.4.2",
+                size: 1,
+                id: "154"
+              },
+              {
+                name: "Cluster 15.4.3",
+                size: 1,
+                id: "155"
+              },
+              {
+                name: "Cluster 15.4.4",
+                size: 1,
+                id: "156"
+              },
+              {
+                name: "Cluster 15.4.5",
+                size: 1,
+                id: "157"
+              },
+              {
+                name: "Cluster 15.4.6",
+                size: 1,
+                id: "158"
+              }
+            ],
+            id: "152"
+          }
+        ],
+        id: "143"
+      }
+    ]
+  }
